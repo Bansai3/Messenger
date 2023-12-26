50e6f70d94370aa0cf9d11e8c6d789708d03c5e7
Bansai3 <931307@bk.ru>
2023-12-26 21:15:03 +0300

    fix: delete unnecessary files

--- build-qtClientTests-Qt_6_6_1_for_macOS-Debug/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/Vadim/Desktop/ITMO/5_сем/Продвинутый_c++/course_project_2/qtClientTests
-# Build directory: /Users/Vadim/Desktop/ITMO/5_сем/Продвинутый_c++/course_project_2/build-qtClientTests-Qt_6_6_1_for_macOS-Debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(clientQtTests "/Users/Vadim/Desktop/ITMO/5_сем/Продвинутый_c++/course_project_2/build-qtClientTests-Qt_6_6_1_for_macOS-Debug/clientQtTests")
-set_tests_properties(clientQtTests PROPERTIES  _BACKTRACE_TRIPLES "/Users/Vadim/Desktop/ITMO/5_сем/Продвинутый_c++/course_project_2/qtClientTests/CMakeLists.txt;31;add_test;/Users/Vadim/Desktop/ITMO/5_сем/Продвинутый_c++/course_project_2/qtClientTests/CMakeLists.txt;0;")
